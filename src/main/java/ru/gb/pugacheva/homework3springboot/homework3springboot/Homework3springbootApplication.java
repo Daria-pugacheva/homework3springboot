ff43c7adc06fe139779c66b22f2a5316c129aaf6
geekbrainsPugachevaD <dpugachev@uandex.ru>
2021-07-30 16:29:07 +0300

    Initial commit

---
+++ src/main/java/ru/gb/pugacheva/homework3springboot/homework3springboot/Homework3springbootApplication.java
@@ -0,0 +1,13 @@
+package ru.gb.pugacheva.homework3springboot.homework3springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework3springbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Homework3springbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/gb/pugacheva/homework3springboot/homework3springboot/Homework3springbootApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gb.pugacheva.homework3springboot.homework3springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework3springbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
